2349d4c5a689e1065d8d69b5cc4617d5eebdbb72
hannalee-ntnu <hanna.lee@ntnu.no>
2024-11-08 10:11:09 +0100

    this is new

---
+++ github_exercise.R
@@ -0,0 +1,15 @@
+install.packages("usethis")
+install.packages("httr2")
+library(usethis)
+library(httr2)
+use_git_config(
+  user.name = "hannalee-ntnu", 
+  user.email = "hanna.lee@ntnu.no"
+)
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+git_vaccinate()
+usethis::use_git()
+use_github()
+git_default_branch_rename()
+git_default_branch_configure(name = "main")
